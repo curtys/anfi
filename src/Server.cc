0ca4d1695f189b48c8819d58fb21ac006ed949ae
Albana Jaha <albanajaha@outlook.com>
2018-11-01 16:26:45 +0100

    Created Server file

---
+++ src/Server.cc
@@ -0,0 +1,105 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "Server.h"
+#include "Job.h"
+#include "SelectionStrategies.h"
+#include "IPassiveQueue.h"
+
+namespace ainf {
+
+Define_Module(Server);
+
+Server::Server()
+{
+    selectionStrategy = nullptr;
+    jobServiced = nullptr;
+    endServiceMsg = nullptr;
+    allocated = false;
+}
+
+Server::~Server()
+{
+    delete selectionStrategy;
+    delete jobServiced;
+    cancelAndDelete(endServiceMsg);
+}
+
+void Server::initialize()
+{
+    busySignal = registerSignal("busy");
+    emit(busySignal, false);
+
+    endServiceMsg = new cMessage("end-service");
+    jobServiced = nullptr;
+    allocated = false;
+    selectionStrategy = SelectionStrategy::create(par("fetchingAlgorithm"), this, true);
+    if (!selectionStrategy)
+        throw cRuntimeError("invalid selection strategy");
+}
+
+void Server::handleMessage(cMessage *msg)
+{
+    if (msg == endServiceMsg) {
+        ASSERT(jobServiced != nullptr);
+        ASSERT(allocated);
+        simtime_t d = simTime() - endServiceMsg->getSendingTime();
+        jobServiced->setTotalServiceTime(jobServiced->getTotalServiceTime() + d);
+        send(jobServiced, "out");
+        jobServiced = nullptr;
+        allocated = false;
+        emit(busySignal, false);
+
+        // examine all input queues, and request a new job from a non empty queue
+        int k = selectionStrategy->select();
+        if (k >= 0) {
+            EV << "requesting job from queue " << k << endl;
+            cGate *gate = selectionStrategy->selectableGate(k);
+            check_and_cast<IPassiveQueue *>(gate->getOwnerModule())->request(gate->getIndex());
+        }
+    }
+    else {
+        if (!allocated)
+            error("job arrived, but the sender did not call allocate() previously");
+        if (jobServiced)
+            throw cRuntimeError("a new job arrived while already servicing one");
+
+        jobServiced = check_and_cast<Job *>(msg);
+        simtime_t serviceTime = par("serviceTime");
+        scheduleAt(simTime()+serviceTime, endServiceMsg);
+        emit(busySignal, true);
+    }
+}
+
+void Server::refreshDisplay() const
+{
+    getDisplayString().setTagArg("i2", 0, jobServiced ? "status/execute" : "");
+}
+
+void Server::finish()
+{
+}
+
+bool Server::isIdle()
+{
+    return !allocated;  // we are idle if nobody has allocated us for processing
+}
+
+void Server::allocate()
+{
+    allocated = true;
+}
+
+}; //namespace
